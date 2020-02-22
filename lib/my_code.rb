33750f5799c6020d76e2952e3287a3fecdfd85ba
Connor Blakeney <connorblakeney@yahoo.com>
2020-02-22 04:49:37 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -51,7 +51,10 @@ end
 def reduce_to_all_true(source_array) 
   i = 0
   while i < source_array.length do
-    source_array[i] true? 
+    source_array[i] true? return true: return false 
+    i += 1
+  end 
+  
 end 
     
   
