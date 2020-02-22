2737f6fce9a31ea256de45225e5acb11d7afffcc
Connor Blakeney <connorblakeney@yahoo.com>
2020-02-22 04:32:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -18,4 +18,13 @@ def map_to_no_change(source_array)
   end 
   new_array
 end
+
+def map_to_double(source_array) 
+  i = 0
+  while i < source_array.length do
+  new_array.push(source_array * 2)
+  i += 1
+  end 
+  new_array
+end 
     
