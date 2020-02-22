55165d652a9d97bb589eaf34188b9ca87e4753e6
Connor Blakeney <connorblakeney@yahoo.com>
2020-02-22 04:20:08 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,10 @@
 # My Code here....
+def map_to_negativize(source_array) 
+  new_array = []
+  i = 0
+  while i < source_array do
+    new_array.push(source_array[i] * -1)
+  end 
+  i += 1
+new_array 
+end 
